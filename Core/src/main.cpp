938192f026302782aeafdeab73e4cd664150c935
Hung Le <conghungletran@gmail.com>
2024-02-12 19:29:12 -0600

    const

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -3,35 +3,46 @@
  * light blinks on the arduino and the the serial output is working. See the README for how to do this.
  */
 #include "Arduino.h"
+#include "const.h"
 #include "sensor.h"
-
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
 
-void setup()
-{
-  std::vector<int> input_pins = {A0, A1, A2, A3};
-  std::vector<int> output_pins = {D9, D10, D11, D12};
+void setup() {
+    for (auto i : IR_INPUT_PINS)
+        pinMode(i, INPUT);
 
-  for (auto i : input_pins)
-    pinMode(i, INPUT);
+    for (auto i : IR_OUTPUT_PINS)
+        pinMode(i, OUTPUT);
 
-  for (auto i : output_pins)
-    pinMode(i, OUTPUT);
-
-  Serial.begin(115200);
+    Serial.begin(115200);
 }
 
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
+void loop() {
+    // digitalWrite(LED_BUILTIN, HIGH);
 
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+    // delay(1000);
+    // digitalWrite(LED_BUILTIN, LOW);
 
-  IR ir_sensor;
-  ir_sensor.print_test();
+    // IR ir_sensor;
+    // ir_sensor.print_test();
 
-  delay(1000);
+    // for (auto input_pin : IR_INPUT_PINS){
+    //     digitalWrite(input_pin, HIGH);
+    // }
+    digitalWrite(D9, HIGH);
+    digitalWrite(D10, HIGH);
+    digitalWrite(D11, HIGH);
+    digitalWrite(D12, HIGH);
+
+    IR ir_sensor;
+    ir_sensor.print_test();
+    for (auto output_pin : IR_OUTPUT_PINS){
+        int val = analogRead(output_pin);
+        Serial.print(val);
+        Serial.print(" ");
+    }
+    Serial.println();
+    delay(50);
 }
