a63a295e98e0d4be688730b1bc089421844277cd
Hung Le <conghungletran@gmail.com>
2024-04-05 13:04:58 -0500

    cooking async

--- Interface/d.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from bleak import discover
-async def scan():
-    devices = await discover()
-    for d in devices:
-        print(d)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(scan())
