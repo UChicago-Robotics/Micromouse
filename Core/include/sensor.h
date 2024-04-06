1a4a6d53cbc9bb05c306107c78213ced08c82801
connorblake1 <70154703+connorblake1@users.noreply.github.com>
2024-04-05 19:34:04 -0500

    Refactored Sensor .h/.cpp

--- Core/include/sensor.h
+++ Core/include/sensor.h
@@ -6,13 +6,16 @@
 class SensorController {
    private:
     int** sensorHistory;  // 2D array for sensor history
-    int L;                // Length of each list
+    int L;                // Length of each list in sensor history
+    float lambda;         // discount rate of exponential smoothing
+    int RR, RF, LF, LL;   // most recent raw sensor values
+    float RRs, RFs, LFs, LLs;
     int currentIndex;     // Index to keep track of the current position to insert new data
     float gx, gy, gz;
     float ax, ay, az;
 
    public:
-    SensorController(int L_val);
+    SensorController(int L_val, double lambda_val);
     void init();
     void allOn();
     void allOff();
@@ -20,11 +23,20 @@ class SensorController {
     void frontOff();
     void sidesOn();
     void sidesOff();
-    void push(int* inIR);
-    double* expSmooth(double lambda);
-    int* readAll();
-    int* readFront();
-    int* readSides();
+    void push(); 
+    void readAll();
+    void readFront();
+    void readSides();
+    int getRR();
+    int getRF();
+    int getLF();
+    int getLL();
+    float getRRs();
+    float getRFs();
+    float getLFs();
+    float getLLs();
     void readIMU();
+    String dumpString();
+
 };
 #endif
--- Core/src/sensor.cpp
+++ Core/src/sensor.cpp
@@ -3,8 +3,17 @@
 #include "Arduino_LSM9DS1.h"
 #include "stdlib.h"
 
-SensorController::SensorController(int L_val) {
+SensorController::SensorController(int L_val, double lambda_val) {
     this->L = L_val;
+    this->lambda = lambda_val;
+    this->RR = 0;
+    this->RF = 0;
+    this->LF = 0;
+    this->LL = 0;
+    this->RRs = 0;
+    this->RFs = 0;
+    this->LFs = 0;
+    this->LLs = 0;
     this->sensorHistory = new int*[4];
     for (int i = 0; i < 4; ++i) {
         this->sensorHistory[i] = new int[L];
@@ -50,40 +59,82 @@ void SensorController::sidesOff() {
     digitalWrite(LL_IRo, LOW);
 }
 
-int* SensorController::readAll() {
-    int* arr = new int[4];
-    arr[0] = analogRead(LF_IRi);
-    arr[1] = analogRead(LL_IRi);
-    arr[2] = analogRead(RR_IRi);
-    arr[3] = analogRead(RF_IRi);
-    return arr;
+void SensorController::readAll() {
+    this->LF = analogRead(LF_IRi);
+    this->LL = analogRead(LL_IRi);
+    this->RR = analogRead(RR_IRi);
+    this->RF = analogRead(RF_IRi);
 }
-int* SensorController::readFront() {
-    int* arr = new int[2];
-    arr[0] = analogRead(LF_IRi);
-    arr[1] = analogRead(RF_IRi);
-    return arr;
+
+int SensorController::getRR() {
+    return this->RR;
 }
-int* SensorController::readSides() {
-    int* arr = new int[2];
-    arr[0] = analogRead(LL_IRi);
-    arr[1] = analogRead(RR_IRi);
-    return arr;
+
+int SensorController::getRF() {
+    return this->RF;
 }
-void SensorController::push(int* inIR) {
+
+int SensorController::getLF() {
+    return this->LF;
+}
+
+int SensorController::getLL() {
+    return this->LL;
+}
+
+float SensorController::getRRs() {
+    return this->LLs;
+}
+
+float SensorController::getRFs() {
+    return this->LLs;
+}
+
+float SensorController::getLFs() {
+    return this->LLs;
+}
+
+float SensorController::getLLs() {
+    return this->LLs;
+}
+
+void SensorController::readFront() {
+    this->LF = analogRead(LF_IRi);
+    this->RF = analogRead(RF_IRi);
+}
+
+void SensorController::readSides() {
+    this->LL = analogRead(LL_IRi);
+    this->RR = analogRead(RR_IRi);
+}
+
+void SensorController::push() {
+    // LF, LL, RR, RF
     for (int i = 0; i < 4; i++) {
         // Shift elements up by 1
         for (int j = L - 1; j > 0; j--) {
             this->sensorHistory[i][j] = this->sensorHistory[i][j - 1];
         }
         // Insert new data into the first slot
-        this->sensorHistory[i][0] = inIR[i];
+        switch(i) {
+            case 0:
+                this->sensorHistory[i][0] = LF;
+                break;
+            case 1:
+                this->sensorHistory[i][0] = LL;
+                break;
+            case 2:
+                this->sensorHistory[i][0] = RR;
+                break;
+            case 3:
+                this->sensorHistory[i][0] = RF;
+                break;
+            default:
+                break;
+        }
     }
     this->currentIndex++;
-}
-
-double* SensorController::expSmooth(double lambda) {
-    double* result = new double[4];
+    // L is how many values it smooths back over, lambda is the discount factor
     int ci = std::min(this->L, this->currentIndex);
     for (int i = 0; i < 4; i++) {
         double weightedSum = 0.0;
@@ -93,24 +144,31 @@ double* SensorController::expSmooth(double lambda) {
             weightedSum += weight * this->sensorHistory[i][j];
             denominator += weight;
         }
-        result[i] = weightedSum / denominator;
+        // I know this looks disgusting but idc -CB
+        switch(i) {
+            case 0:
+                LFs = weightedSum / denominator;
+                break;
+            case 1:
+                LLs = weightedSum / denominator;
+                break;
+            case 2:
+                RRs = weightedSum / denominator;
+                break;
+            case 3:
+                RFs = weightedSum / denominator;
+                break;
+            default:
+                break;
+        }
     }
-    return result;
 }
 
 void SensorController::readIMU() {
     IMU.readAcceleration(this->ax, this->ay, this->az);
     IMU.readGyroscope(this->gx, this->gy, this->gz);
-    Serial.print(this->ax);
-    Serial.print(" ");
-    Serial.print(this->ay);
-    Serial.print(" ");
-    Serial.print(this->az);
-    Serial.print(" ");
-    Serial.print(this->gx);
-    Serial.print(" ");
-    Serial.print(this->gy);
-    Serial.print(" ");
-    Serial.println(this->gz);
-    String accelString = String(this->ax) + "," + String(this->ay) + "," + String(this->az) + "," + String(this->gx) + "," + String(this->gy) + "," + String(this->gz);
+}
+String SensorController::dumpString() {
+    // LFs,LLs,RRs,RFs,ax,ay,az,gx,gy,gz with 2 decimals of precision
+    return String(this->LFs,2) + "," + String(this->LLs,2) + "," + String(this->RRs,2) + "," + String(this->RFs,2) + "," + String(this->ax,2) + "," + String(this->ay,2) + "," + String(this->az,2) + "," + String(this->gx,2) + "," + String(this->gy,2) + "," + String(this->gz,2);
 }
